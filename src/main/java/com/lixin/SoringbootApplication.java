5aac252ab78f1f5060a1b038c5bb428f70f825e0
lixin <javahuiyishi.com>
2020-12-12 16:49:51 +0800

    测试idea

---
+++ src/main/java/com/lixin/SoringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoringbootApplication.class, args);
+    }
+
+}
