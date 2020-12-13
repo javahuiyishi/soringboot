651a6b06fb2e236f7fdd80181ccda4df0c4cac9a
lixin <javahuiyishi.com>
2020-12-13 22:15:21 +0800

    测试idea

---
+++ src/main/java/com/lixin/controller/XcxAction.java
@@ -0,0 +1,47 @@
+package com.lixin.controller;
+
+
+public class XcxAction {
+
+    private long id;
+    private String name;
+    private long moduleId;
+    private String actionName;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public long getModuleId() {
+    return moduleId;
+  }
+
+  public void setModuleId(long moduleId) {
+    this.moduleId = moduleId;
+  }
+
+
+  public String getActionName() {
+    return actionName;
+  }
+
+  public void setActionName(String actionName) {
+    this.actionName = actionName;
+  }
+
+}
