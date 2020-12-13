4fc27deeb7af8e86db0c6421f4d16d2844bd6104
lixin <javahuiyishi.com>
2020-12-13 22:13:42 +0800

    测试idea

--- src/main/java/com/lixin/controller/login.java
+++ src/main/java/com/lixin/controller/login.java
@@ -1,14 +1,16 @@
 package com.lixin.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class login {
 
-    @RequestMapping("main")
-    public  String test(){
+    @RequestMapping(value = "main",method = RequestMethod.POST)
+    public  String test(String name){
+        String a ="www";
+        System.out.println(a);
         return "1111";
     }
 }
